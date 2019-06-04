ff0cf09e3727a7578699d8ff84cc6954b4035284
Viola Crotts <vyley@pop-os.localdomain>
2019-06-03 22:29:53 -0400

    real commit

---
+++ currently unused/welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	Welcome <?php echo $_POST["name"]; ?> <br>
+	Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
